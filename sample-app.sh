215fe2be05382ce95789e1a1e599a088b1036968
GersoZz <gersonzuniga@uni.pe>
2024-09-28 18:23:37 +0000

    refactor: restructure project to use NodeJS

--- sample-app.sh
+++ sample-app.sh
@@ -4,17 +4,27 @@ mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
-cp sample_app.py tempdir/.
+cp sample_app.js tempdir/.
+cp package.json tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "# Usa una imagen base de Node.js" >> tempdir/Dockerfile
+echo "FROM node:16-slim" >> tempdir/Dockerfile
+echo "# Establece el directorio de trabajo en el contenedor" >> tempdir/Dockerfile
+echo "WORKDIR /home/myapp" >> tempdir/Dockerfile
+echo "# Copia el archivo package.json y package-lock.json (si existe)" >> tempdir/Dockerfile
+echo "COPY package*.json ./" >> tempdir/Dockerfile
+echo "# Instala las dependencias de la aplicación" >> tempdir/Dockerfile
+echo "RUN npm install" >> tempdir/Dockerfile
+echo "# Copia los archivos de la aplicación" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  sample_app.js /home/myapp/" >> tempdir/Dockerfile
+echo "# Expone el puerto 8080 para el servidor web" >> tempdir/Dockerfile
 echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "# Comando para iniciar la aplicación" >> tempdir/Dockerfile
+echo "CMD [\"node\", \"sample_app.js\"]" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
---
+++ sample_app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+
+// Sirve los archivos estáticos desde la carpeta 'static'
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+// Configura el directorio de plantillas
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');  // Usaremos EJS como el motor de plantillas
+
+// Ruta principal que renderiza el archivo 'index.ejs'
+app.get('/', (req, res) => {
+
+    console.log(req.ip)
+    console.log(req.connection.remoteAddress)
+    const remoteAddr = req.ip || req.connection.remoteAddress;
+
+    res.render('index',{ remoteAddr });  // Esto buscará el archivo 'index.ejs' en la carpeta 'templates'
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://0.0.0.0:${PORT}`);
+});
